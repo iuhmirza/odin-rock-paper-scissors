2ed317129bf8e846e99a3ec32fb997b3184c8d64
ibtisam <iuhmirza@gmail.com>
2022-08-01 15:33:22 +0100

    Refactor unused code

--- rps.js
+++ rps.js
@@ -2,9 +2,6 @@ const rock = 1
 const paper = 2
 const scissors = 3
 
-const rockButton = document.querySelector('#rock');
-const paperButton = document.querySelector('#paper');
-const scissorsButton = document.querySelector('#scissors');
 const dialog = document.createElement('div');
 const body = document.querySelector('body')
 document.body.appendChild(dialog);
@@ -15,19 +12,6 @@ let computerScore = 0;
 function getComputerChoice() {
     choice = Math.floor(Math.random() * 3 + 1)
     return choice
-}
-
-function getPlayerChoice () {
-    choice = prompt("Rock/Paper/Scissors")
-    choice = choice.toUpperCase()
-    switch(choice) {
-        case "ROCK":
-            return rock
-        case "PAPER":
-            return paper
-        case "SCISSORS":
-            return scissors
-    }
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -64,11 +48,3 @@ function playRound(playerSelection, computerSelection) {
         computerScore = 0
     }
 }
-/*
-function game() {
-    let winCount = 0
-    for(let i = 0; i<5; i++) {
-        winCount += playRound(getPlayerChoice(), getComputerChoice())
-    } if (winCount > 0) { return "You Win!"} else {return "get good scrub"}
-}
-*/
